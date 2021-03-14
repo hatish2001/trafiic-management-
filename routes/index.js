27cfb410d085b6d43c724442929fceb54d652845
hatish2001 <48691395+hatish2001@users.noreply.github.com>
2021-03-14 10:32:09 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,254 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Nexmo = require('nexmo');
+var bodyparser=require('body-parser');
+const app = express();
+router.use(bodyparser.urlencoded({extended:true}));
 
+
+let User = require('../models/user');
+let Complaint = require('../models/complaint');
+let ComplaintMapping = require('../models/complaint-mapping');
+
+// Home Page - Dashboard
+router.get('/', ensureAuthenticated, (req, res, next) => {
+    res.render('index');
+});
+
+// Login Form
+router.get('/login', (req, res, next) => {
+    res.render('login');
+});
+
+// Register Form
+router.get('/register', (req, res, next) => {
+    res.render('register');
+});
+
+// Logout
+router.get('/logout', ensureAuthenticated,(req, res, next) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/login');
+});
+
+// Admin
+router.get('/admin', ensureAuthenticated, (req,res,next) => {
+    Complaint.getAllComplaints((err, complaints) => {
+        if (err) throw err;
+    
+        User.getEngineer((err, engineer) => {
+            if (err) throw err;
+
+            res.render('admin/admin', {
+                complaints : complaints,
+                engineer : engineer,
+            });
+        });
+    });        
+});
+
+
+// Assign the Complaint to Engineer
+router.post('/assign', (req,res,next) => {
+    const complaintID = req.body.complaintID;
+    const engineerName = req.body.engineerName;
+    req.flash('success_msg', 'You have successfully assigned a complaint to Engineer');
+    res.redirect('/admin');
+      
+});
+
+
+//Complaint
+router.get('/complaint', ensureAuthenticated, (req, res, next) => {
+    //console.log(req.session.passport.username);
+    //console.log(user.name);
+    res.render('complaint', {
+        username: req.session.user,
+    });
+});
+
+//Register a Complaint
+router.post('/registerComplaint', (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const contact = req.body.contact;
+    const desc = req.body.desc;
+    
+    const postBody = req.body;
+    console.log(postBody);
+
+    req.checkBody('contact', 'Contact field is required').notEmpty();
+    req.checkBody('desc', 'Description field is required').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('complaint', {
+            errors: errors
+        });
+    } else {
+        const newComplaint = new Complaint({
+            name: name,
+            email: email,
+            contact: contact,
+            desc: desc,
+        });
+
+        Complaint.registerComplaint(newComplaint, (err, complaint) => {
+            if (err) throw err;
+            req.flash('success_msg', 'You have successfully launched a complaint');
+            res.redirect('/');
+        });
+    }
+});
+
+
+
+// Process Register
+router.post('/register', (req, res, next) => {
+    const name = req.body.name;
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+    const role = req.body.role;
+
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email must be a valid email address').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    req.checkBody('role', 'Role option is required').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        const newUser = new User({
+            name: name,
+            username: username,
+            email: email,
+            password: password,
+            role: role
+        });
+
+        User.registerUser(newUser, (err, user) => {
+            if (err) throw err;
+            req.flash('success_msg', 'You are Successfully Registered and can Log in');
+            res.redirect('/login');
+        });
+    }
+});
+
+// Local Strategy
+passport.use(new LocalStrategy((username, password, done) => {
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return done(null, false, {
+                message: 'No user found'
+            });
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, {
+                    message: 'Wrong Password'
+                });
+            }
+        });
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    var sessionUser = {
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+    }
+    done(null, sessionUser);
+});
+
+passport.deserializeUser((id, done) => {
+    User.getUserById(id, (err, sessionUser) => {
+        done(err, sessionUser);
+    });
+});
+
+// Login Processing
+router.post('/login', passport.authenticate('local', 
+    { 
+        failureRedirect: '/login', 
+        failureFlash: true 
+    
+    }), (req, res, next) => {
+    
+        req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        if(req.user.role==='admin'){
+            res.redirect('/admin');
+        }
+        else if(req.user.role==='jeng'){
+            res.redirect('/jeng');
+        }
+        else{
+            res.redirect('/');
+        }
+    });
+});
+router.get("/sms",function(req,res){
+    res.render("sms.ejs");
+});
+
+
+router.post("/sms",(req,res)=>{
+    const nexmo = new Nexmo({
+    apiKey: '09854cae',
+    apiSecret:'iarFfss5RhYLwUeD'
+})
+    let text = req.body.subject
+ 
+    nexmo.message.sendSms("Traffic management", "918189844293", text, {
+    type: "unicode"
+},  (err, responseData) => {
+    if (err) {
+        console.log(err);
+    }else {
+        if (responseData.messages[0]['status'] === "0") {
+            console.log("Message sent successfully.");
+             req.flash('success_msg', 'You are Successfully Registered and can Log in');
+
+    } else {
+        console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+    }
+  }
+})
+    res.redirect("/sms");
+
+});
+
+
+// Access Control
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = router;
